22bdce2aca6954d45519e7b06195cfefd686505b
Khatravat Goutham Rathod <khatravathgoutham@gmail.com>
2026-02-01 10:38:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ function App() {
           <ScrollToTop />
           <Switch>
             <Route path="/" exact component={Home} />
-            <Route path="/" exact component={Experience}/>
+            <Route path="/experience" exact component={Experience}/>
             <Route path="/project" component={Projects} />
             <Route path="/about" component={About} />
             <Route path="/resume" component={Resume} />
