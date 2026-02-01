ea093cb483ad909b71da46020b99aef07f0ec3b4
Khatravat Goutham Rathod <khatravathgoutham@gmail.com>
2026-02-01 10:34:11 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,6 @@ import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
-import { AiOutlineUser, AiOutlineHome } from "react-icons/ai";
 import { MdWorkOutline } from "react-icons/md";
 
 import {
